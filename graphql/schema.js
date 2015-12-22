a499cb454b4e95d77150df240c90c52bce8f7c8d
Jed Watson <jed.watson@me.com>
2015-12-22 12:26:36 +1100

    Adding the schedule to the GraphQL Schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -2,6 +2,7 @@ var GraphQL = require('graphql');
 var keystone = require('keystone');
 var keystoneTypes = require('keystone-graphql').Types;
 
+var schedule = require('../lib/schedule');
 var Person = keystone.list('Person');
 var Talk = keystone.list('Talk');
 
@@ -10,7 +11,7 @@ var personType = new GraphQL.GraphQLObjectType({
 	fields: () => ({
 		id: {
 			type: new GraphQL.GraphQLNonNull(GraphQL.GraphQLID),
-			description: 'The id of the user.',
+			description: 'The id of the Person',
 		},
 		name: { type: keystoneTypes.Name(Person.fields.name) },
 		email: { type: GraphQL.GraphQLString },
@@ -28,17 +29,35 @@ var talkType = new GraphQL.GraphQLObjectType({
 	fields: () => ({
 		id: {
 			type: new GraphQL.GraphQLNonNull(GraphQL.GraphQLID),
-			description: 'The id of the user.',
+			description: 'The id of the Talk',
 		},
 		title: { type: GraphQL.GraphQLString },
 		type: { type: GraphQL.GraphQLString },
+		time: keystoneTypes.Datetime(Talk.fields.time),
 		person: {
 			type: personType,
 			resolve: (source) =>
 				Person.model.findById(source.person).exec(),
 		},
-		time: keystoneTypes.Datetime(Talk.fields.time),
 		description: { type: keystoneTypes.Markdown(Talk.fields.description) },
+	}),
+});
+
+var scheduleType = new GraphQL.GraphQLObjectType({
+	name: 'ScheduleItem',
+	fields: () => ({
+		key: {
+			type: new GraphQL.GraphQLNonNull(GraphQL.GraphQLID),
+			description: 'The key used to link times to talks',
+		},
+		label: { type: GraphQL.GraphQLString },
+		type: { type: GraphQL.GraphQLString },
+		date: { type: GraphQL.GraphQLString },
+		description: { type: GraphQL.GraphQLString },
+		duration: {
+			type: GraphQL.GraphQLInt,
+			desciption: 'The number of minutes this schedule item goes for',
+		},
 	}),
 });
 
@@ -61,6 +80,11 @@ module.exports = new GraphQL.GraphQLSchema({
 				},
 				resolve: (_, args) =>
 					Person.model.findById(args.id).exec(),
+			},
+			schedule: {
+				type: new GraphQL.GraphQLList(scheduleType),
+				resolve: (_, args) =>
+					schedule.times,
 			},
 			talks: {
 				type: new GraphQL.GraphQLList(talkType),
